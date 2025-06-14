8cb99ec028b8fb8446c60fb2cc6a3e246961d76f
Rufus Sommer <rufussommer@gmx.de>
2025-06-14 12:51:05 +0200

    Updates

---
+++ kotlin/SupplyChainProblem.kt
@@ -0,0 +1,151 @@
+package solutions
+
+import core.Solution
+import solutions.SupplyChainProblem.Node.Companion.names
+import kotlin.math.max
+
+data class TableRow(
+    var unaffected: List<String>,
+    var affected: List<String>,
+    var dependent: List<String>
+)
+
+fun Collection<TableRow>.toText(): String =
+    map { Triple(it.unaffected.joinToString(","), it.affected.joinToString(","), it.dependent.joinToString(",")) }.run {
+        val longest: Triple<Int, Int, Int> = fold(Triple(10, 8, 9)) { acc, row ->
+            Triple(max(acc.first, row.first.length), max(acc.second, row.second.length), max(acc.third, row.third.length))
+        }
+        "unaffected${" ".repeat(longest.first - 10)} | affected${" ".repeat(longest.second - 8)} | dependent${" ".repeat(longest.third - 9)}\n" +
+                joinToString("\n") { "${it.first}${" ".repeat(longest.first - it.first.length)} | ${it.second}${" ".repeat(longest.second - it.second.length)} | ${it.third}${" ".repeat(longest.third - it.third.length)}" }
+    }
+
+inline fun Boolean.ifAlso(lam: () -> Any?): Boolean = this.also { if (this) lam() }
+inline fun Boolean.elseAlso(lam: () -> Any?): Boolean = this.also { if (!this) lam() }
+
+object SupplyChainProblem: Solution
+{
+    override fun entryPoint(vararg args: String)
+    {
+        val table = try { args[0].uppercase().split("\n").map { row ->
+            val split = row.split("|").map { it.uppercase().split(",").filter { str -> str.isNotEmpty() } }
+            TableRow(split[0], split[1], split[2])
+        } } catch (e: IndexOutOfBoundsException) { throw Exception("wrong input format", e) }
+
+        println(table.toText())
+        findGraph(table)
+    }
+
+    class Node(
+        val name: String,
+        var targets: MutableList<Node> = arrayListOf(),
+        //val sources: ArrayList<Node> = arrayListOf(),
+        var affects: MutableList<Node> = arrayListOf(),
+        var dependent: MutableList<Node> = arrayListOf(),
+    )
+    {
+        val finished = false
+        val dependAndAffected get() = dependent + affects
+        fun unaffected(of: List<Node>): List<Node> = ArrayList(of).apply { removeAll(affects); removeAll(dependent); remove(this@Node) }
+        fun measureDependAffect(): ArrayList<Node> = ArrayList<Node>().apply {
+            fun ArrayList<Node>.recursive(start: Node)
+            {
+                for (i in start.targets)
+                {
+                    if (i in this) continue
+                    add(i)
+                    recursive(i)
+                }
+            }
+            recursive(this@Node)
+        }
+        override fun toString(): String = "Node $name: targets: ${targets.names()}, affects: ${affects.names()}, dependent: ${dependent.names()}"
+        companion object
+        {
+            public fun Collection<Node>.names() = map { it.name }
+        }
+    }
+    class Graph(
+        val nodes: ArrayList<Node> = arrayListOf(),
+    )
+    {
+        fun addNode(name: String): Node =
+            nodes.find { it.name == name } ?: Node(name).apply { nodes.add(this) }
+        fun addNode(node: Node) =
+            nodes.find { it.name == node.name } ?: nodes.add(node)
+        fun allConnections(): List<String> = nodes.fold(listOf()) { acc, node -> acc + node.targets.names().map { node.name + it } }
+        operator fun get(name: String): Node? = nodes.find { it.name == name }
+        override fun toString(): String = "${super.toString()}: nodes: \n${nodes.joinToString("\n")}"
+    }
+
+
+    fun findGraph(table: List<TableRow>)
+    {
+        val all: HashMap<String, Node> = hashMapOf(*table.first().run { unaffected + affected + dependent }.map { it to Node(it) }.toTypedArray())
+        fun doForNew(node: Node, row: TableRow, graph: Graph)
+        {
+            node.affects.addAll(row.affected.map { all[it]!! })
+            node.dependent.addAll(row.dependent.filter { it != node.name }.map { all[it]!! })
+            graph.addNode(node)
+            /*
+            if (row.unaffected.size == 1) all[row.unaffected.first()]!!.targets.add(node)
+            if (row.dependent.size == 2) node.targets.add(all[row.dependent[1]]!!)
+
+             */
+        }
+        val graph = Graph().apply {
+            var ref = mutableMapOf(*table.mapIndexed { index, row -> index to row.dependent }.toTypedArray())
+            for (reps in 1..table.size)
+            {
+                ref = ref.filter {
+                    (it.value.size != 1).elseAlso {
+                        doForNew(all[it.value.first()]!!, table[it.key], this)
+                    }
+                }.toMutableMap()
+                ref.forEach { (index, value) ->
+                    ref[index] = value.filter { it !in nodes.names() }
+                }
+                if (ref.filter { it.value.size == 1 }.isEmpty()) break
+            }
+            if (ref.isNotEmpty()) throw Exception("this table is none of my presets")
+
+            println(this)
+
+            nodes.forEach { node ->
+                node.targets.addAll(node.affects.filter { affected ->
+                    node.dependAndAffected.none { affected in it.dependAndAffected }
+                })
+                node.targets.addAll(node.dependent.filter { affected ->
+                    node.dependent.none { affected in it.dependAndAffected }
+                })
+            }
+            nodes.forEach { node ->
+                node.affects.forEach { affected ->
+                    if (node.affects.none { affected in it.dependAndAffected })
+                        node.unaffected(all.values.toList()).filter { affected in it.dependAndAffected && node !in it.dependAndAffected }.let { ref ->
+                            val reduced = ref.filter { refEl ->
+                                refEl.dependAndAffected.none { it in (ref - refEl) }
+                                //(ref - refEl).none { refEl in it.dependAndAffected }
+                            }
+                            reduced.forEach { it.targets.add(affected) }
+//                            if (reduced.size == 1)
+//                                reduced.first().targets.add(affected)
+                        }
+
+                    /*
+                    if (affected !in node.measureDependAffect())
+                        node.targets.add(affected)
+
+                    // */
+                }
+            }
+
+
+
+            println(this)
+            println(this.allConnections().joinToString(", "))
+
+        }
+    }
+
+
+}
